46420edcdf316a3298aabe29124d880069aa7ad9
Hung Ngo <hungngo95nt@gmail.com>
2018-11-16 01:34:40 +0700

    Update database

---
+++ server/database/index.js
@@ -0,0 +1,9 @@
+
+const Model = {
+    Locate:require('./locate'),
+    Driver:require('./driver'),
+    Phone:require('./phone'),
+    Point:require('./point'),
+    User:require('./user')
+}
+module.exports = Model
