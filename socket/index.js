bf2403ca82b90ee789818bc1cac29a5aae682699
Hung Ngo <hungngo95nt@gmail.com>
2018-11-16 01:40:57 +0700

    Implement socket

--- socket/index.js
+++ socket/index.js
@@ -21,7 +21,23 @@ let Phoners = new Map()
 let LocationsQueue = []
 
 
-
+io.on('connection', (socket) => {
+    //==============Connection=========== 
+    socket.on(Client.DRIVER, (client_id) => {
+        console.log(`Connected: ${client_id}`)
+        OnlineDriver(client_id)
+            .then(response => {
+                for (let [key, value] of Locaters) {
+                    io.to(value.id).emit(Driver.ONLINE, response.data.message)
+                }
+            })
+            .catch(err => console.error(err))
+        Drivers.set(client_id, {
+            id: socket.id,
+            status: "free"
+        })
+    })
+})
 server.listen(8000, (err) => {
     if (err) throw err
     console.log("Socket is running")
