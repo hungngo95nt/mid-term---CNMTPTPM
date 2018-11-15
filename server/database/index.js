0426eb861ebb515a8c33e7b7b6225f79fe9ab69e
Hung Ngo <hungngo95nt@gmail.com>
2018-11-16 01:33:40 +0700

    Re create data

--- server/database/index.js
+++
@@ -1,9 +0,0 @@
-
-const Model = {
-    Locate:require('./locate'),
-    Driver:require('./driver'),
-    Phone:require('./phone'),
-    Point:require('./point'),
-    User:require('./user')
-}
-module.exports = Model
