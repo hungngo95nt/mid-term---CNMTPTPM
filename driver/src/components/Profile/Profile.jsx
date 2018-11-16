cc219d3ab677ed5af49a6eec2e088638151cb1a2
Hung Ngo <hungngo95nt@gmail.com>
2018-11-16 19:07:06 +0700

    Create profile page

---
+++ driver/src/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+    console.log("profile")
+    return (
+        <div>
+            this is profile page
+        </div>
+    );
+};
+
+export default Profile;
