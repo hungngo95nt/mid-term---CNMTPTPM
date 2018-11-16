c06a2f24fbd29775772272c97e055b7efeb10714
Hung Ngo <hungngo95nt@gmail.com>
2018-11-16 19:07:30 +0700

    Create call history page

---
+++ driver/src/components/CallHistory/CallHistory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CallHistory = () => {
+    console.log("his")
+    return (
+        <div>
+            this is history page
+        </div>
+    );
+};
+
+export default CallHistory;
