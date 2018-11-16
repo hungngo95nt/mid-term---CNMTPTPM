8f330e2e9eaa7018b9561d840fe130eb98419cc5
Hung Ngo <hungngo95nt@gmail.com>
2018-11-16 19:14:47 +0700

    Fix bug related to service worker

--- driver/src/registerServiceWorker.js
+++ driver/src/registerServiceWorker.js
@@ -82,11 +82,7 @@ function checkValidServiceWorker(swUrl) {
         response.headers.get('content-type').indexOf('javascript') === -1
       ) {
         // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then(registration => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
-        });
+
       } else {
         // Service worker found. Proceed as normal.
         registerValidSW(swUrl);
